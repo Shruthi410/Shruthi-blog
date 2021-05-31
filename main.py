1da30a9c610ba03ae179cd0b0772be33c0505526
shrut <shruthid0410@gmail.com>
2021-05-31 21:24:13 +0530

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ my_email = "shruthi41098@gmail.com"
 my_password = "Shruthigmail123"
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
